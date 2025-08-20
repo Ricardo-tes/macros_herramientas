7338ce521e85d1f585d99727e04b83f8045bb4ac
ricardo-tes <rmc.jimenez@gmail.com>
2025-08-20 19:33:13 -0300

    primeros  cambios

--- herramientas.py
+++ herramientas.py
@@ -6,50 +6,52 @@ from pynput.keyboard import Key, Listener
 def on_press(key):
     if key==Key.enter:
         return False
-
-
-def tomar_zona():
-
-# Permite delimitar una zona rectangular de la pantalla indicando dos esquinas, la superior izquierda y la inferior derecha. La funcion muestra una captura
+def esperar_enter():
+#Detecta cuando se presiona enter sin necesidad de que el programa esté en foco. Ignorará cualquier otra tecla
+    with Listener(on_press=on_press) as listener:
+            listener.join()
+#La funcion muestra una captura
 # del area seleccionada. Se puede guardar la captura o descartarla. Si el area es correcta para el usuario la retorna, sino retorna None. 
-#
+
+def medir_zona():
+# Permite delimitar una zona rectangular de la pantalla indicando dos esquinas, la superior izquierda y la inferior derecha. Retorna la región en formato diccionario
+    zona={}
     print("Ubique el puntero del mouse en la esquina superior izquierda y oprima enter")
-    with Listener(on_press=on_press) as listener: #Detecta cuando se presiona enter sin necesidad de que el programa esté en foco
-        listener.join()
+    esperar_enter()
     six, siy =pyautogui.position()
     print("Ubique el puntero del mouse en la esquina inferior derecha y oprima enter")
-    with Listener(on_press=on_press) as listener:
-        listener.join()
+    esperar_enter()
     idx, idy =pyautogui.position()
-    ancho = abs(idx-six)
-    alto = abs(idy-siy)
-    zona = (six, siy, ancho, alto)
-    captura = pyautogui.screenshot(region=zona)
-    captura.show()
-    nombre=input("\nSi quiere guardar la capura ingrese el nombre. De lo contrario presione enter ")
-    if nombre != "":
-        nombre=nombre+".png"
-        captura.save(nombre, "PNG")
-    confirma=input("\nEl area delimitada es la correcta? s/n Enter ")
-    if confirma.lower() =='s':
+    zona["ancho"] = abs(idx-six)
+    zona["alto"] = abs(idy-siy)
+    zona["x"]=six
+    zona["y"]=siy
+    return (zona)
+
+    # captura = pyautogui.screenshot(region=zona)
+    # captura.show()
+    # nombre=input("\nSi quiere guardar la capura ingrese el nombre. De lo contrario presione enter ")
+    # if nombre != "":
+    #     nombre=nombre+".png"
+    #     captura.save(nombre, "PNG")
+    # confirma=input("\nEl area delimitada es la correcta? s/n Enter ")
+    # if confirma.lower() =='s':
 
         
-        return zona
-    else:
-        return None
+    #     return zona
+    # else:
+    #     return None
 
 def tomar_punto():
-#Toma coordenas xy de un punto de la pantalla
-    input("\nPosicione el puntero donde quiera tomar la posicio y presione enter")
-    punto = pyautogui.position()
-    confirma = input(f"Coordenadas: \nX: {punto.x} \nY: {punto.y} \nGuardar punto? s/n Enter ")
-    if confirma.lower()=='s':
-        return (punto)
-    else:
-        return None
+#Toma coordenas xy de un punto de la pantalla y lo retorna en formato cordenada x, cordenada y
+    print("\nPosicione el puntero donde quiera tomar la posicio y presione enter")
+    with Listener(on_press=on_press) as listener: #Detecta cuando se presiona enter sin necesidad de que el programa esté en foco
+        listener.join()
+    return (pyautogui.position())
 
 
-
+def guardar_zona():
+    zona=medir_zona()
 
 
 # punto = tomar_punto()
@@ -59,10 +61,10 @@ def tomar_punto():
 #         puntos[etiqueta]=punto
 
  
-zona = tomar_zona()
-if zona != None:
-    print(zona)
-    etiqueta=input("\nIngrese nombre para etiquetar la zona guardada ")
-    with shelve.open('zonas', 'c') as base_zona:
-        base_zona[etiqueta] = zona
+# zona = tomar_zona()
+# if zona != None:
+#     print(zona)
+#     etiqueta=input("\nIngrese nombre para etiquetar la zona guardada ")
+#     with shelve.open('zonas', 'c') as base_zona:
+#         base_zona[etiqueta] = zona
     
